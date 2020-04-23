257bc8a2395ee4b33619566ee6a71933db526b83
MauriceLakey <49058489+MauriceLakey@users.noreply.github.com>
2020-04-23 16:42:12 +0200

    Updates

---
+++ precache-manifest.6018779512f3de61a7d3a4f03381141b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b3b11901a2b93ec5e119100177a554",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "fa6d6d5ed99a5e4eaf62",
+    "url": "/deploy-react/static/js/2.164e5bae.chunk.js"
+  },
+  {
+    "revision": "e295f6174982883eeaf5d0be2126d2ea",
+    "url": "/deploy-react/static/js/2.164e5bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "089af77bb1d1f5650535",
+    "url": "/deploy-react/static/js/main.28ec7642.chunk.js"
+  },
+  {
+    "revision": "597c83e40b9675500ad5754445b3bb19",
+    "url": "/deploy-react/static/js/main.28ec7642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7689ac5656ff0fbc2d0",
+    "url": "/deploy-react/static/js/runtime-main.bb4fb35e.js"
+  }
+]);
